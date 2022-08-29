451a0e0676c485705a945bdbc89c247471a81dfa
SRIDHAR DHARMAVARAPU <sridhardharmavarapu@MBPvanSeheerder.home>
2022-08-30 00:09:10 +0200

    feat(products): Update products component layout and styling

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductsService } from '@dpg/shared/services/products.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
+  protected title = 'Product list';
+  protected productsData: any = [];
+  private destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.productsService
+      .getProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response: any) => {
+        if (response.data.length) {
+          this.productsData = response.data;
+        }
+      });
   }
 
+  addToCart(product: any) {
+    console.log('Product added to cart::', product);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
