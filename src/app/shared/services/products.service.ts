3f217802c544c8f277827136c92b29fb878a8160
SRIDHAR DHARMAVARAPU <sridhardharmavarapu@MBPvanSeheerder.home>
2022-08-30 00:12:29 +0200

    feat(products): Update products service to fetch products list

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private productsApi = environment.productsApiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  // TODO: Define interface for products data
+  getProducts(): Observable<any[]> {
+    return this.http.get<any[]>(this.productsApi)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    
+    return throwError(
+      () => new Error(errorMessage)
+    );
+  }
+  
 }
