61302642fab513308dfbaba47ac1b2291d79c39e
SRIDHAR DHARMAVARAPU <sridhardharmavarapu@MBPvanSeheerder.home>
2022-08-30 12:39:40 +0200

    feat(products): Update styles and error handling

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductCategory } from '@dpg/shared/interfaces/product-category.interface';
+import { Products } from '@dpg/shared/interfaces/products.interface';
 import { ProductsService } from '@dpg/shared/services/products.service';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -8,8 +10,9 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit, OnDestroy {
-  protected title = 'Product list';
-  protected productsData: any = [];
+  protected title = 'Join the AD';
+  protected productsData!: Array<ProductCategory>;
+  protected errorMessage = '';
   private destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private productsService: ProductsService) {}
@@ -18,15 +21,17 @@ export class ProductsComponent implements OnInit, OnDestroy {
     this.productsService
       .getProducts()
       .pipe(takeUntil(this.destroy$))
-      .subscribe((response: any) => {
-        if (response.data.length) {
-          this.productsData = response.data;
-        }
+      .subscribe({
+        next: (response: Products) => {
+          if (response.data.length) {
+            this.productsData = response.data;
+          }
+        },
+        error: (error) => {
+          debugger;
+          this.errorMessage = error;
+        },
       });
-  }
-
-  addToCart(product: any) {
-    console.log('Product added to cart::', product);
   }
 
   ngOnDestroy() {
